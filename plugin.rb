417bc821e09494af2dc0b387862768744066321f
paully21 <papostolos@allpaul.com>
2016-02-06 12:52:20 -0600

    Made change to top in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -67,12 +67,13 @@ after_initialize do
         end
       end
 
-      top = SiteContent.content_for(:notification_email_top)
+      #top = SiteContent.content_for(:notification_email_top)
 
       html = UserNotificationRenderer.new().render(
         file: '/plugins/sifhmail/app/views/email/notification',
         format: :html,
-        locals: { context_posts: context_posts, post: post, top: top ? PrettyText.cook(top).html_safe : nil }
+		locals: { context_posts: context_posts, post: post }
+        #locals: { context_posts: context_posts, post: post, top: top ? PrettyText.cook(top).html_safe : nil }
       )
 
       template = "user_notifications.user_#{notification_type}"
